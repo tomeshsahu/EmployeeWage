ac00d0089ff3c55c395fbb8e401e9a7aee42c8d8
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-27 15:54:32 +0530

    [TOMESHWAR]ADD:-EMPLOYEE WAGE MONTH 100 HRS

--- EmployeeWage/UC6-EmpWage.cs
+++ EmployeeWage/UC6-EmpWage.cs
@@ -6,19 +6,21 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage
 {
-    public class UC5_EmpWage
+    public class UC6_EmpWage
     {
-        public static void main(string[] args)
+        public static void Main(String[] args)
         {
             //Constant
             const int Is_FullTime = 1;
             const int Is_PartTime = 2;
             const int EmpWagePerHrs = 20;
             const int EmpWorkingDays = 20;
+            const int EmpMaxWorkingHrs = 100;
             //Variable
             int EmpHrs = 0;
             int EmpWage = 0;
             int totalEmpHrs = 0;
+            int Salary = 0;
             for (int i = 1; i <= EmpWorkingDays; i++)
             {
                 Random random = new Random();
@@ -37,10 +39,15 @@ namespace EmployeeWage
                 }
                 EmpWage = EmpHrs * EmpWagePerHrs;
                 Console.WriteLine($"Day{i}= {EmpWage}");
-                totalEmpHrs +=EmpHrs;
+                totalEmpHrs = totalEmpHrs + EmpHrs;
+                if(totalEmpHrs<=100)
+                {
+                    Salary = totalEmpHrs * EmpWagePerHrs;
+                }
+
             }
-            
-            Console.WriteLine($"Total Salary= {totalEmpHrs * EmpWagePerHrs}");
+            Console.WriteLine($"Total Hrs = {totalEmpHrs}");
+            Console.WriteLine($"Total Salary= {Salary}");
         }
     }
 }
