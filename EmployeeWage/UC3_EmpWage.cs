a94f640916f3f240ce118db1bcbfb4ecb3538594
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-27 15:08:19 +0530

    [TOMESHWAR]ADD:-EMPLOYEE WAGE PART TIME

--- EmployeeWage/UC3_EmpWage.cs
+++ EmployeeWage/UC3_EmpWage.cs
@@ -6,26 +6,31 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage
 {
-    public class UC2_EmployeeWage
+    public class UC3_EmpWage
     {
-        public static void main(String[] args)
+        public static void Main(String[] args)
         {
             //Constant
             const int Is_FullTime = 1;
+            const int Is_PartTime = 2;
             const int EmpWagePerHrs = 20;
             //Variable
-            int EmpHrs=0;
-            int EmpWage=0;
+            int EmpHrs = 0;
+            int EmpWage = 0;
 
             Random random = new Random();
             int check = random.Next(0, 2);
-            if(check==Is_FullTime)
+            if (check == Is_FullTime)
             {
-                EmpHrs= 8;
+                EmpHrs = 8;
+            }
+            else if(check==Is_PartTime)
+            {
+                EmpHrs = 4;
             }
             else
             {
-                Console.WriteLine("Employee Is Absent");
+                EmpHrs = 0;
             }
             Console.WriteLine($"Employee Wage= { EmpHrs * EmpWagePerHrs}");
         }
