312e4c5a42e6efa9547ffd3ffd4af8d56a0b13d5
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-27 13:06:48 +0530

    [Tomeshwar]ADD-EMPLOYEE ATTENDANCE

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    public class UC1_EmplyeeWage
+    {
+        const int Is_FullTime = 1;
+        public static void Main(String[] args)
+        {
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == Is_FullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
